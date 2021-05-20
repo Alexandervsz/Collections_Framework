3a08aa80dd4dd1f9103fe8953a390fe0c8bd7a45
Alexandervsz <alexander_5560@hotmail.com>
2021-05-20 15:43:47 +0200

    - Added bubbleSort algorithm, which sorts randomInts. - Added alphabetize, which translates the ints into characters and stores them in an string-list. - Added main function for testing.

--- Main/src/RandomStringGenerator.java
+++ Main/src/RandomStringGenerator.java
@@ -2,13 +2,40 @@ import java.util.*;
 
 public class RandomStringGenerator {
     private final Random random = new Random();
+    private final List<Integer> randomInts = new ArrayList<>();
     private final List<String> randomString = new ArrayList<>();
 
-    public List<String> getRandomString(int length) {
+    public List<String> generateRandomString(int length) {
         for (int x = 0; x < length; x++) {
-            int randomNumber = random.nextInt(26) + 1;
+            randomInts.add(random.nextInt(26) + 1);
+        }
+        bubbleSort();
+        alphabetize();
+        return randomString;
+    }
+
+    private void bubbleSort() {
+        int length = randomInts.size();
+        int storageInt;
+        for (int x = 0; x < length; x++) {
+            for (int y = 1; y < (length - x); y++) {
+                if (randomInts.get(y - 1) > randomInts.get(y)) {
+                    storageInt = randomInts.get(y - 1);
+                    randomInts.set(y - 1, y);
+                    randomInts.set(y, storageInt);
+                }
+            }
+        }
+    }
+
+    private void alphabetize() {
+        for (int randomNumber : randomInts) {
             randomString.add(String.valueOf((char) (randomNumber + 96)));
         }
-        return randomString;
+    }
+
+    public static void main(String[] args) {
+        RandomStringGenerator rsg = new RandomStringGenerator();
+        System.out.println(rsg.generateRandomString(1));
     }
 }
